d2303de5772a6d2db8c87a1c02ffeb3b5c940a05
ivan minutillo <minutillo.ivan@gmail.com>
2018-06-01 00:25:54 +0200

    components update

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
 import { configure } from '@storybook/react';
+import { setAddon, addDecorator } from '@storybook/react';
+import JSXAddon from 'storybook-addon-jsx';
+setAddon(JSXAddon);
 
 const req = require.context('../lib/components', true, /\.stories\.js$/)
 
 function loadStories() {
+  require('./welcomeStory');
   req.keys().forEach((filename) => req(filename))
 }
 
